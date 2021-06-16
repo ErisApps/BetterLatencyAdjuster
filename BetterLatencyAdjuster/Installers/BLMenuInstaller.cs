c712ff6af6a86726af4ec0c7bed77cf272407658
Eris <erisapps@outlook.be>
2021-06-16 21:58:40 +0200

    Kinda rewrote the mod entirely

---
+++ BetterLatencyAdjuster/Installers/BLMenuInstaller.cs
@@ -0,0 +1,25 @@
+﻿using BetterLatencyAdjuster.Settings.UI;
+using IPA.Logging;
+using SiraUtil;
+using Zenject;
+
+namespace BetterLatencyAdjuster.Installers
+{
+	internal class BLMenuInstaller : Installer<Logger, BLMenuInstaller>
+	{
+		private readonly Logger _logger;
+
+		public BLMenuInstaller(Logger logger)
+		{
+			_logger = logger;
+		}
+
+		public override void InstallBindings()
+		{
+			Container.BindLoggerAsSiraLogger(_logger);
+
+			Container.BindInterfacesAndSelfTo<SettingsController>().AsSingle();
+			Container.BindInterfacesTo<SettingsControllerManager>().AsSingle();
+		}
+	}
+}
---
+++ BetterLatencyAdjuster/Settings/UI/SettingsControllerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using BeatSaberMarkupLanguage.Settings;
+using Zenject;
+
+namespace BetterLatencyAdjuster.Settings.UI
+{
+	internal class SettingsControllerManager : IInitializable, IDisposable
+	{
+		private SettingsController? _settingsHost;
+
+		[Inject]
+		public SettingsControllerManager(SettingsController settingsHost)
+		{
+			_settingsHost = settingsHost;
+		}
+
+		public void Initialize()
+		{
+			BSMLSettings.instance.AddSettingsMenu("<size=85%>BetterLatencyAdjuster", "BetterLatencyAdjuster.Settings.UI.Settings.bsml", _settingsHost);
+		}
+
+		public void Dispose()
+		{
+			if (_settingsHost == null)
+			{
+				return;
+			}
+
+			BSMLSettings.instance.RemoveSettingsMenu(_settingsHost);
+			_settingsHost = null!;
+		}
+	}
+}
